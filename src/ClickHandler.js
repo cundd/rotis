4f97aa12c88ec5d3eb7983d1e1842c831b19943d
Daniel Corn <d@cundd.net>
2016-07-20 21:55:56 +0200

    Fixed a bug where clicking a lone cell scored

--- src/ClickHandler.js
+++ src/ClickHandler.js
@@ -17,11 +17,15 @@ class ClickHandler {
     _updateState(grid, connectedCells) {
         let stateBefore = this.store.getState();
         const previousScore = stateBefore.score;
+        let newScore = previousScore;
+        if (connectedCells.length > 1) {
+            newScore = previousScore + Math.pow(2, connectedCells.length);
+        }
 
         const newState = Object.assign({}, stateBefore, {
             grid: grid,
             columns: grid.getData(),
-            score: previousScore + Math.pow(2, connectedCells.length)
+            score: newScore
         });
 
         this.store.setState(newState);
