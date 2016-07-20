dca8ba7beb9f28f9e07e4083496e00cd1095999e
Daniel Corn <d@cundd.net>
2016-07-20 21:09:04 +0200

    Small enhancement to the version item

--- src/View/Version.jsx
+++ src/View/Version.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class extends React.Component {
     render() {
-        return <div className="toolbar-item -right">Version <span className="version">{this.props.version}</span></div>
+        return <div className="toolbar-item -right version">v{this.props.version}</div>
     }
 };
