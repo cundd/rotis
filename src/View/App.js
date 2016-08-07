e085dfadb73608c16206f5a21a30144e8b4a7e70
Daniel Corn <d@cundd.net>
2016-08-07 13:34:38 +0200

    Code cleanup

--- src/View/App.js
+++ src/View/App.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-// import logo from '../logo.svg';
-// import '../App.css';
 import Grid from '../Grid';
 import GameWindow from './GameWindow';
 
@@ -30,22 +28,7 @@ class App extends Component {
 
             clickHandler={this.props.clickHandler}
         />;
-
-
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
     }
-
-
 
     restart() {
         this.props.store.reset();
