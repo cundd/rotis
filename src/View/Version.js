606fdc9fa66930bc008e32dc53827484a304875a
Daniel Corn <d@cundd.net>
2016-08-12 12:30:44 +0200

    Added short info screen on version click

--- src/View/Version.js
+++ src/View/Version.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Modal from './Modal';
 
 export default class extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showModal: false
+        }
+    }
+
+    onModalClick(event) {
+        event.stopPropagation();
+        this.setState({showModal: false});
+    }
+
+    onVersionClick() {
+        this.setState({showModal: true});
+    }
+
     render() {
-        return <div className="toolbar-item -right version">v{this.props.version}</div>
+        const _onModalClick = this.onModalClick.bind(this);
+        const _onVersionClick = this.onVersionClick.bind(this);
+
+        let modal;
+        if (this.state.showModal) {
+            modal = <Modal heading="Rotis" onClick={_onModalClick}>&copy; Daniel Corn 2016</Modal>
+        }
+
+        return <div className="toolbar-item -right version" onClick={_onVersionClick}>
+            v{this.props.version}
+            {modal}
+        </div>;
     }
 };
